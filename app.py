d61669878a589ef8e3b6b6fdc2cdc7fc39e7522a
Patrick <patasax22@yahoo.com>
2023-07-13 16:04:50 -0400

    added buttons and functionallity for adding and deleting list items

--- app.py
+++ app.py
@@ -49,5 +49,43 @@ my_scrollbar.pack(side=RIGHT, fill=BOTH)
 my_list.config(yscrollcommand=my_scrollbar.set)
 my_scrollbar.config(command=my_list.yview)
 
+# entry box to add items to list
+my_entry = Entry(root, font=("Freestyle"))
+my_entry.pack(pady=20)
+
+# create button frame
+button_frame = Frame(root)
+button_frame.pack(pady=20)
+
+
+# functions
+def delete_item():
+    my_list.delete(ANCHOR)
+
+
+def add_item():
+    my_list.insert(END, my_entry.get())
+    my_entry.delete(0, END)
+
+
+def cross_off_item():
+    pass
+
+
+def uncross_item():
+    pass
+
+
+# add buttons
+delete_button = Button(button_frame, text="Delete Item", command=delete_item)
+add_button = Button(button_frame, text="Add Item", command=add_item)
+cross_off_button = Button(button_frame, text="Cross Off Item", command=cross_off_item)
+uncross_button = Button(button_frame, text="Uncross Item", command=uncross_item)
+
+delete_button.grid(row=0, column=0)
+add_button.grid(row=0, column=1, padx=20)
+cross_off_button.grid(row=0, column=2)
+uncross_button.grid(row=0, column=3, padx=20)
+
 
 root.mainloop()
